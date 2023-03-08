6c4dce3712adf4567ceefa726ac167a7fcd1e007
Ryan Adi Prasetyo <127333304+RyanAprs@users.noreply.github.com>
2023-03-09 00:01:07 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// SILDE
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+    var imgList = document.getElementsByClassName('about-image');
+
+    if(n > imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length;
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = 'none';
+    }
+
+    imgList[slideIndex - 1].style.display = 'block';
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 2000);
+
+
+
+// VALIDATE FORM
+function validateform() {
+    var name = document.forms['contact-form']['name'].value;
+    var email = document.forms['contact-form']['email'].value;
+    var select = document.forms['contact-form']['select'].value;
+
+    if (name == '' || email == '' || select == '') {
+        alert('Input tidak boleh kosong!!');
+        return false;
+    }
+}
